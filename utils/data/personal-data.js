c27ba2ad91e12d9a6ad461446936ece2c73389c4
Karbal Hamza <karbalhamza96@gmail.com>
2025-01-25 00:11:02 +0100

    Info Modif

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "KARBAL Hamza",
-  profile: '/photo.png',
+  profile: '/profile.png',
   designation: "Software Developer",
   description: "My name is KARBAL Hamza. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'karbalhamza96@gmail.com',
